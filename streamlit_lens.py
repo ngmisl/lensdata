3a8be42df076ca2aa7c5950ee2c13867db947ec7
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-06-26 11:00:16 +0200

    updates

--- streamlit_lens.py
+++ streamlit_lens.py
@@ -1,4 +1,3 @@
-from asyncore import write
 import json
 
 import requests as re
@@ -42,28 +41,40 @@ def flatten_json(y):
 
 st.write(""" # Lens Data """)
 
+
+def sideBar():
+    with st.sidebar:
+        st.markdown("**Follow Me**")
+        st.markdown("* [ngmisl.lens](https://lenster.xyz/u/ngmisl.lens)")
+        st.markdown("* [ngmisl.twitter](https://twitter.com/ngmisl)")
+        st.markdown("* [ngmisl.github](https://github.com/ngmisl/)")
+
+
 def totalProtocol():
-  json_data = json.loads(r.text)
-  flat = flatten_json(json_data)
-  
-  # Layout Start
-  
-  col1, col2, col3, col4, col5 = st.columns(5)
+    json_data = json.loads(r.text)
+    flat = flatten_json(json_data)
 
-  with col1:
-    col1.write("Total Profiles")
-    col1.write(flat["data_globalProtocolStats_totalProfiles"])
-  with col2:
-    col2.write("Total Posts")
-    col2.write(flat["data_globalProtocolStats_totalPosts"])
-  with col3:
-    col3.write("Total Comments")
-    col3.write(flat["data_globalProtocolStats_totalComments"])
-  with col4:
-    col4.write("Total Collects")
-    col4.write(flat["data_globalProtocolStats_totalCollects"])
-  with col5:
-    col5.write("Total Mirrors")
-    col5.write(flat["data_globalProtocolStats_totalMirrors"])
+    # Layout Start
+    # Layout references: https://docs.streamlit.io/library/api-reference/layout
 
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        col1.write("Total Profiles")
+        col1.write(flat["data_globalProtocolStats_totalProfiles"])
+    with col2:
+        col2.write("Total Posts")
+        col2.write(flat["data_globalProtocolStats_totalPosts"])
+    with col3:
+        col3.write("Total Comments")
+        col3.write(flat["data_globalProtocolStats_totalComments"])
+    with col4:
+        col4.write("Total Collects")
+        col4.write(flat["data_globalProtocolStats_totalCollects"])
+    with col5:
+        col5.write("Total Mirrors")
+        col5.write(flat["data_globalProtocolStats_totalMirrors"])
+
+
+sideBar()
 totalProtocol()
